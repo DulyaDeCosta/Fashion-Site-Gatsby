b45da21908d759c7f0ad41719ccd891e843dc6fd
DulyaDeCosta <dulyasanuvi@gmail.com>
2024-07-19 01:37:55 +0530

    Footer and Navbar styles updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,14 +17,16 @@ const Footer = () => {
         </div>
         <div className="row text-center my-2">
           <div className="col-sm-12 text-center text-white">
-            <img src={logo} alt="Maxi Fashion House" />
+            <img src={logo} alt="Maxi Fashion House" className="img-fluid my-3" />
             Maxi Fashion House
           </div>
-          <div className="col-sm-12" style={{ color: 'darkgray' }}>Equipped to serve the most distinctive tastes, we provide bridal dresses for all occasions to make your special moments even more memorable.</div>
+          <div className="col-sm-12" style={{ color: 'darkgray' }}>
+            Equipped to serve the most distinctive tastes, we provide bridal dresses for all occasions to make your special moments even more memorable.
+          </div>
         </div>
         <div className="row text-center my-2">
           <div className="col">
-            <a href="https://www.facebook.com/maxifashionhouse/" target="_blank" className="no-text-decor" style={{ color: 'darkgray' }}>
+            <a href="/" className="no-text-decor" style={{ color: 'darkgray' }}>
               <i className="fab fa-facebook fa-lg mx-2"></i>
             </a>
             <i className="fab fa-twitter fa-lg mx-2" style={{ color: 'darkgray' }}></i>
@@ -32,14 +34,14 @@ const Footer = () => {
         </div>
         <div className="row my-2">
           <div className="col-sm-12 text-center" style={{ color: 'darkgray' }}>
-            <p style={{ color: 'darkgray' }}>© 2024 Maxi Fashion House. All rights reserved. <br />
-            </p><hr style={{ borderTop: '1px solid darkgray', filter: 'drop-shadow(0px 0px 3px white)' }} />
-            Designed &amp; developed by <a href="https://www.chanukawijayakoon.me" target="_blank">merovingienne</a>.
+            <p style={{ color: 'darkgray' }}>© {new Date().getFullYear()} Maxi Fashion House. All rights reserved.<br />
+              </p><hr style={{ borderTop: '1px solid darkgray', filter: 'drop-shadow(0px 0px 3px white)' }} />
+              Designed &amp; developed by <a href="/">merovingienne</a>.
           </div>
         </div>
       </div>
     </footer>
   );
-};
+}
 
 export default Footer;
